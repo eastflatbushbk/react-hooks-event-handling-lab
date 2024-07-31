695952e4872302ec71fdfbbaf6e0c21664ba800d
Robert Sainton <robertsainton@outlook.com>
2024-07-31 17:55:17 -0400

    brush up

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -2,7 +2,7 @@
 import React from "react";
 
 function EyesOnMe (){
-    return <button onFocus={() => {console.log('Good!');}} 
+    return <button onFocus={() => {console.log('Good!')}} 
         onBlur={()=>{console.log('Hey! Eyes on me!')}} >Eyes on me</button>;
 }
 export default EyesOnMe
